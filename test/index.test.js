e46d17dfa99850feebd802574ecf86f3195e7a30
zhouhuafei <zhouhuafei@shopex.cn>
2018-03-01 15:24:53 +0800

    3

--- test/index.test.js
+++ test/index.test.js
@@ -2,5 +2,6 @@ const cookie = require('../dist/index.min');
 
 test(`cookie操作`, () => {
     cookie.setCookie('hello', 'world');
-    console.log( cookie.getCookie('hello')); // 'world'
+    console.log(cookie.getCookie('hello')); // 'world'
+    expect(cookie.getCookie('hello')).toEqual('world');
 });
